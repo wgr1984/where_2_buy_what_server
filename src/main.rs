fa6ec797b2f7797fdbacf0664a4356089cd3e738
wgr1984 <w.reithmeier@gmail.com>
2020-04-13 17:58:12 +0200

    get port from env

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::env;
 
 mod routes {
     pub mod greet;
 }
 
 use routes::greet::{greet};
+
+fn get_port() -> u16 {
+    if let Ok(port_var) = env::var("PORT") {
+        if let Ok(port) = port_var.parse::<u16>() {
+            return port;
+        }
+    }
+    8080
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -13,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(format!("127.0.0.1:{}", get_port()))?
     .run()
     .await
 }
